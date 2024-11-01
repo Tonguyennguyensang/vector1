07bb08671215d2018b864af66bb93ba29ab3db81
Tonguyennguyensang <tonguyennguyensangcode@gmail.com>
2024-11-01 23:45:04 +0700

    set.cpp

---
+++ set.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vetor>
+#include <set>
+using namespace std;
+int main(){
+ ios_base::sync_with_stio(0):cin.tie(0);cout.tie(0);
+  set<int> s;
+  s.insert(10);
+  s.insert(9);
+  s.insert(1);
+  for(auto x : s){
+  cout << x <<" "; // tập hợp từ nhỏ đến lớn
+} multise t<int> s;
+  s.insert(10);
+  s.insert(9);
+  s.insert(10);
+  cout << s.size <<" ";
+  for(auto x : s)
+  cout << x <<" ";
+  
+   
+
+}
